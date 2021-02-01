55940cdb977fe4c6c7e2419dfc3a17680dde6974
ganghun.cho <ganghun.cho@nts-corp.com>
2021-02-02 00:14:56 +0900

    [init] 스프링 부트

---
+++ src/main/java/com/chopa/wdyw/WdywApplication.java
@@ -0,0 +1,13 @@
+package com.chopa.wdyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdywApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chopa/wdyw/WdywApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chopa.wdyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdywApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
